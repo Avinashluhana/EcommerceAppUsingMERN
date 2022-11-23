d265c27a4aa1cb1156601b1d11d1d1f5026292a6
Avinash kumar <49323775+Avinashluhana@users.noreply.github.com>
2022-11-23 20:42:38 +0500

    solved redirecting bug

--- frontend/src/component/Route/ProtectedRoute.js
+++ frontend/src/component/Route/ProtectedRoute.js
@@ -2,16 +2,19 @@ import React, { Fragment } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = ({isAdmin}) => {
+const ProtectedRoute = ({ isAdmin }) => {
   const { loading, isAuthenticated, user } = useSelector((state) => state.user);
 
   return (
     <Fragment>
-      {loading ===false &&
-        (isAuthenticated === false  && isAdmin === false && user.role !== "admin" ? <Navigate to="login" /> : <Outlet />)  
-
-        }
-        {/* {loading ===false &&
+      {loading === false &&
+        (isAuthenticated === false ? <Navigate to="login" /> : <Outlet />)}
+      {isAdmin === true && user.role !== "admin" ? (
+        <Navigate to="login" />
+      ) : (
+        <></>
+      )}
+      {/* {loading ===false &&
         (isAdmin === true && user.role !== "admin" ? <Navigate to="login" /> : <Outlet />)  
         } */}
     </Fragment>
